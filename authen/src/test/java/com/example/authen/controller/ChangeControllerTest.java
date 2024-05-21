3d2244a0e3235b03f4050c3f7b10ed1880315d6d
eulerBM <eullerborgesdamotta155@gmail.com>
2024-05-21 19:21:21 -0300

    Adicionando tests

---
+++ authen/src/test/java/com/example/authen/controller/ChangeControllerTest.java
@@ -0,0 +1,24 @@
+package com.example.authen.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ChangeControllerTest {
+
+    @Test
+    void changePassword() {
+    }
+
+    @Test
+    void changeUsername() {
+    }
+
+    @Test
+    void changeRole() {
+    }
+
+    @Test
+    void changeLanguage() {
+    }
+}
