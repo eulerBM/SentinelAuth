289e34e2abfe98a762e6860a44bf398bd0709e3f
eulerBM <eullerborgesdamotta155@gmail.com>
2024-07-03 23:15:21 -0300

    Docker ajustes

--- authen/src/main/java/com/example/authen/AuthenApplication.java
+++ authen/src/main/java/com/example/authen/AuthenApplication.java
@@ -7,8 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AuthenApplication {
 
 	public static void main(String[] args) {
+
+
 		
 		SpringApplication.run(AuthenApplication.class, args);
+
 
 	}
 }
